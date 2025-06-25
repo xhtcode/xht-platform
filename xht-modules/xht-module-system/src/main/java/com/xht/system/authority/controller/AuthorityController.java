29146bc847bee91f433d3b22b6a0a9804b793814
小糊涂 <616326125@qq.com>
2025-06-25 21:23:53 +0800

    新增加xht-framework-resource-server 模块

--- xht-modules/xht-module-system/src/main/java/com/xht/system/authority/controller/AuthorityController.java
+++ xht-modules/xht-module-system/src/main/java/com/xht/system/authority/controller/AuthorityController.java
@@ -7,6 +7,7 @@ import com.xht.system.authority.service.IAuthorityService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +32,7 @@ public class AuthorityController {
      * @return 用户信息
      */
     @Operation(summary = "获取用户信息")
-    @RequestMapping("/user/profile/info")
+    @GetMapping("/user/profile/info")
     public R<AuthorityUserVO> getUserProfileInfo() {
         return R.ok(authorityService.getUserProfileInfo());
     }
@@ -43,7 +44,7 @@ public class AuthorityController {
      * @return 路由信息
      */
     @Operation(summary = "获取路由信息")
-    @RequestMapping("/user/profile/getRouters")
+    @GetMapping("/user/profile/getRouters")
     public R<List<INode<Long>>> getRouters() {
         return R.ok(authorityService.getRouters());
     }
